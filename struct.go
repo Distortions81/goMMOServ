4fdbc3539fe435dce49865f436c6b56a086c7396
Distortions81 <carlotto81@gmail.com>
2023-10-18 18:59:46 -0600

    world structs

--- struct.go
+++ struct.go
@@ -1,13 +1,57 @@
 package main
 
 import (
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+const xyHalf = 2147483648
+
+var xyCenter XY = XY{X: xyHalf, Y: xyHalf}
+
 type playerData struct {
-	conn     *websocket.Conn
-	id       uint32
+	conn *websocket.Conn
+	id   uint32
+
+	name     string
+	location locationData
+
+	inventory []objectData
+
 	lastPing time.Time
+	lock     sync.Mutex
+}
+
+type locationData struct {
+	pos XY
+
+	areaid uint32
+
+	areaP       *areaData
+	superChunkP *superChunk
+	chunkP      *chunkData
+}
+
+type XY struct {
+	X uint32
+	Y uint32
+}
+
+type areaData struct {
+	superChunks []superChunk
+}
+
+type superChunk struct {
+	chunks []chunkData
+}
+
+type chunkData struct {
+	objects []*objectData
+}
+
+type objectData struct {
+	name string
+	uid  uint32
 }
